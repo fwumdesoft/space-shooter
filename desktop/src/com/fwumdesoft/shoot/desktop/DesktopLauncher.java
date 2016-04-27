62e19183ac6b450e71517e250ec2ac1451ce0795
Jason Carrete <jasoncarrete5@gmail.com>
2016-04-27 13:38:20 -0400

    Added server functionality to the DesktopLauncher

--- desktop/src/com/fwumdesoft/shoot/desktop/DesktopLauncher.java
+++ desktop/src/com/fwumdesoft/shoot/desktop/DesktopLauncher.java
@@ -1,12 +1,22 @@
 package com.fwumdesoft.shoot.desktop;
 
+import com.badlogic.gdx.backends.headless.HeadlessApplication;
+import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.fwumdesoft.shoot.Main;
+import com.fwumdesoft.shoot.server.Server;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Main(), config);
+	public static void main(String[] arg) {
+		if(arg.length > 0 && arg[0].equals("server")) {
+			HeadlessApplicationConfiguration config = new HeadlessApplicationConfiguration();
+			config.renderInterval = -1f;
+			new HeadlessApplication(new Server(), config);
+		} else {
+			LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+			config.title = "Shooter";
+			new LwjglApplication(new Main(), config);
+		}
 	}
 }
