2f005ed7197a81923b9e6f98760415f6886e3d33
Jason Carrete <jasoncarrete5@gmail.com>
2016-04-27 13:12:15 -0400

    Added ServerInterface.send(byte[]) method

--- core/src/com/fwumdesoft/shoot/net/ServerInterface.java
+++ core/src/com/fwumdesoft/shoot/net/ServerInterface.java
@@ -11,6 +11,9 @@ import com.badlogic.gdx.Gdx;
 
 /**
  * Allows clients to send/receive messages to the server.
+ * <li>The first byte in each packet identifies the message.
+ * <li>
+ * <li>
  */
 public class ServerInterface {
 	private static final String HOST = "45.33.68.145";
@@ -28,24 +31,26 @@ public class ServerInterface {
 	 */
 	public static boolean connect() {
 		if(connected) {
-			
+			disconnect();
+			connect();
 		} else {
 			try {
 				socket = new DatagramSocket();
 				
 				//set socket options
-				socket.setReceiveBufferSize(10);
-				socket.setSendBufferSize(10);
+				socket.setReceiveBufferSize(256);
+				socket.setSendBufferSize(256);
 				socket.setSoTimeout(5000);
 				
 				InetAddress remoteHost = InetAddress.getByName(HOST);
 				socket.connect(remoteHost, PORT);
 				connected = true;
 				
-				packet = new DatagramPacket(new byte[1024], 1024, remoteHost, PORT);
+				packet = new DatagramPacket(new byte[256], 256, remoteHost, PORT);
 				byte[] data = receive();
+				Gdx.app.log("Connect Test", Byte.toString(data[0]));
 			} catch(SocketException e) {
-				Gdx.app.error("ServerInterface.connect()", "Failed to connect to server", e);
+				Gdx.app.error("ServerInterface.connect()", "Failed to connect to instantiate DatagramSocket", e);
 				connected = false;
 			} catch(UnknownHostException e) {
 				Gdx.app.error("ServerInterface.connect()", "Unknown host address", e);
@@ -53,6 +58,23 @@ public class ServerInterface {
 			}
 		}
 		return connected;
+	}
+	
+	/**
+	 * Sends data to the server.
+	 * @param buf data buffer
+	 * @return <tt>true</tt> if the packet sent successfully, otherwise <tt>false</tt>.
+	 */
+	public static boolean send(byte[] buf) {
+		if(!connected) throw new IllegalStateException("Not connected to the server");
+		packet.setData(buf);
+		try {
+			socket.send(packet);
+			return true;
+		} catch (IOException e) {
+			Gdx.app.log("ServerInterface.send(byte[])", "Failed to send a packet", e);
+			return false;
+		}
 	}
 	
 	/**
@@ -69,8 +91,11 @@ public class ServerInterface {
 				byte[] buf = new byte[packet.getLength()];
 				System.arraycopy(packet.getData(), packet.getOffset(), buf, 0, packet.getLength());
 				return buf;
+			} catch(SocketException e) {
+				Gdx.app.log("ServerInterface.receive(int)", "Most likely caused by a call to disconnect", e);
+				break;
 			} catch(IOException e) {
-				Gdx.app.log("ServerInterace.receive(int)", "Skipped a packet. Trying again, attempt " + (i+1), e);
+				Gdx.app.log("ServerInterface.receive(int)", "Skipped a packet. Trying again, attempt " + (i+1), e);
 			}
 		}
 		return null;
@@ -84,9 +109,11 @@ public class ServerInterface {
 		return connected;
 	}
 	
-	public static void dispose() {
-		socket.close();
+	public static void disconnect() {
+		if(!connected) return;
+		socket.disconnect();
 		socket = null;
+		packet = null;
 		connected = false;
 	}
 }
