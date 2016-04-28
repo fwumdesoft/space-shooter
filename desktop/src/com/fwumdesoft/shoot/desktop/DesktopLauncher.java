0ac5e988a01c7e26e99687a19ffb84834e79848d
Jason C <jasoncarrete5@gmail.com>
2016-04-28 09:12:25 -0400

    Added ability to log any error to file for the server

--- desktop/src/com/fwumdesoft/shoot/desktop/DesktopLauncher.java
+++ desktop/src/com/fwumdesoft/shoot/desktop/DesktopLauncher.java
@@ -1,18 +1,29 @@
 package com.fwumdesoft.shoot.desktop;
 
+import java.io.PrintWriter;
+
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.headless.HeadlessApplication;
 import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.files.FileHandle;
 import com.fwumdesoft.shoot.Main;
 import com.fwumdesoft.shoot.server.Server;
 
 public class DesktopLauncher {
 	public static void main(String[] arg) {
 		if(arg.length > 0 && arg[0].equals("server")) {
-			HeadlessApplicationConfiguration config = new HeadlessApplicationConfiguration();
-			config.renderInterval = -1f;
-			new HeadlessApplication(new Server(), config);
+			try {
+				HeadlessApplicationConfiguration config = new HeadlessApplicationConfiguration();
+				config.renderInterval = -1f;
+				new HeadlessApplication(new Server(), config);
+			} catch(Exception e) {
+				if(Gdx.files != null) {
+					FileHandle logFile = Gdx.files.local("log");
+					e.printStackTrace(new PrintWriter(logFile.writer(true), true));
+				}
+			}
 		} else {
 			LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 			config.title = "Shooter";
