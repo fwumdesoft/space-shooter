5072a782de82c26b620b4bd1c2c99f8e4afff4ed
Jason Carrete <jasoncarrete5@gmail.com>
2016-04-30 18:26:57 -0400

    Scrapped all netcode and restarting. (attempted suicide)

--- core/src/com/fwumdesoft/shoot/net/NetMessage.java
+++
@@ -1,8 +0,0 @@
-package com.fwumdesoft.shoot.net;
-
-public class NetMessage {
-	public static final byte DISCONNECT = (byte)0x01;
-	public static final byte CONNECT = (byte)0x02;
-	public static final byte HEARTBEAT = (byte)0x03;
-	public static final byte UPDATE_PLAYER = (byte)0x04;
-}
