8dec7aab78e6c31a81546784efbe5884721e989f
Jason Carrete <jasoncarrete5@gmail.com>
2016-05-01 18:52:24 -0400

    Added NetActor and Player classes

---
+++ core/src/com/fwumdesoft/shoot/NetActor.java
@@ -0,0 +1,21 @@
+package com.fwumdesoft.shoot;
+
+import java.util.UUID;
+
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+/**
+ * An Actor that has a netId so it can be associated with an Object on the server.
+ */
+public class NetActor extends Actor {
+	public final UUID netId;
+	
+	public NetActor(final UUID id) {
+		netId = id;
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString() + " {" + netId + "}";
+	}
+}
---
+++ core/src/com/fwumdesoft/shoot/Player.java
@@ -0,0 +1,28 @@
+package com.fwumdesoft.shoot;
+
+import java.util.UUID;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+/**
+ * Represents a player in the game that can move around the world.
+ * Players are controlled by each client and the client primarily has
+ * authority over the position of the player.
+ */
+public class Player extends NetActor {
+	private TextureRegion texture;
+	
+	public Player(final UUID id) {
+		super(id);
+		texture = new TextureRegion(Main.assets.get("player.png", Texture.class));
+		setWidth(texture.getRegionWidth());
+		setHeight(texture.getRegionHeight());
+	}
+
+	@Override
+	public void draw(Batch batch, float parentAlpha) {
+		batch.draw(texture, getX(), getY(), getOriginX(), getOriginY(), getWidth(), getHeight(), getScaleX(), getScaleY(), getRotation());
+	}
+}
