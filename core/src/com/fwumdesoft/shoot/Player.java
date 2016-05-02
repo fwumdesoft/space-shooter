eec74be0ec215a4ec3e913cb014daea314e173fb
Jason C <jasoncarrete5@gmail.com>
2016-05-02 18:24:38 -0400

    Added isLocalPlayer instance variable to Player class and updated the server on the Player's position each time it's position is changed

--- core/src/com/fwumdesoft/shoot/Player.java
+++ core/src/com/fwumdesoft/shoot/Player.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.fwumdesoft.shoot.net.ServerInterface;
 
 /**
  * Represents a player in the game that can move around the world.
@@ -12,7 +13,10 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
  * authority over the position of the player.
  */
 public class Player extends NetActor {
+	public static final float SPEED = 5;
+	
 	private TextureRegion texture;
+	private boolean isLocalPlayer;
 	
 	public Player(final UUID id) {
 		super(id);
@@ -20,9 +24,27 @@ public class Player extends NetActor {
 		setWidth(texture.getRegionWidth());
 		setHeight(texture.getRegionHeight());
 	}
+	
+	public Player(final UUID id, boolean isLocalPlayer) {
+		this(id);
+		this.isLocalPlayer = isLocalPlayer;
+	}
 
 	@Override
 	public void draw(Batch batch, float parentAlpha) {
 		batch.draw(texture, getX(), getY(), getOriginX(), getOriginY(), getWidth(), getHeight(), getScaleX(), getScaleY(), getRotation());
+	}
+	
+	@Override
+	public void positionChanged() {
+		if(isLocalPlayer()) {
+			if(ServerInterface.isConnected()) {
+				ServerInterface.updateLocalPlayer(this);
+			}
+		}
+	}
+	
+	public boolean isLocalPlayer() {
+		return isLocalPlayer;
 	}
 }
