5c0c6d0eaf4fd5beb1041f0af30083460a1a68f9
Jason Carrete <jasoncarrete5@gmail.com>
2016-05-06 17:59:30 -0400

    removed useless try catch

--- desktop/src/com/fwumdesoft/shoot/desktop/DesktopLauncher.java
+++ desktop/src/com/fwumdesoft/shoot/desktop/DesktopLauncher.java
@@ -1,29 +1,18 @@
 package com.fwumdesoft.shoot.desktop;
 
-import java.io.PrintWriter;
-
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.headless.HeadlessApplication;
 import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.badlogic.gdx.files.FileHandle;
 import com.fwumdesoft.shoot.Main;
 import com.fwumdesoft.shoot.net.server.Server;
 
 public class DesktopLauncher {
 	public static void main(String[] arg) {
 		if(arg.length > 0 && arg[0].equals("server")) {
-			try {
-				HeadlessApplicationConfiguration config = new HeadlessApplicationConfiguration();
-				config.renderInterval = 0f; //this will keep the main loop alive
-				new HeadlessApplication(new Server(), config);
-			} catch(Throwable e) {
-				if(Gdx.files != null) {
-					FileHandle logFile = Gdx.files.local("log");
-					e.printStackTrace(new PrintWriter(logFile.writer(true), true));
-				}
-			}
+			HeadlessApplicationConfiguration config = new HeadlessApplicationConfiguration();
+			config.renderInterval = 0f; //this will keep the main loop alive
+			new HeadlessApplication(new Server(), config);
 		} else {
 			LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 			config.title = "Shooter";
