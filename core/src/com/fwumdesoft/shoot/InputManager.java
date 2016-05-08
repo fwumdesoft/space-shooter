2b255de605129f6ecc582e9a05b29475eb048d6c
Jason Carrete <jasoncarrete5@gmail.com>
2016-05-07 22:11:49 -0400

    Now scaling movement by deltaTime

--- core/src/com/fwumdesoft/shoot/InputManager.java
+++ core/src/com/fwumdesoft/shoot/InputManager.java
@@ -1,5 +1,6 @@
 package com.fwumdesoft.shoot;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.math.Vector2;
@@ -9,7 +10,6 @@ import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.actions.MoveByAction;
 import com.badlogic.gdx.scenes.scene2d.actions.RepeatAction;
-import com.badlogic.gdx.utils.Pool;
 import com.badlogic.gdx.utils.Pools;
 import com.fwumdesoft.shoot.model.Bolt;
 import com.fwumdesoft.shoot.model.Player;
@@ -20,7 +20,6 @@ import com.fwumdesoft.shoot.net.ServerInterface;
  */
 public class InputManager extends InputListener {
 	private RepeatAction moveForward, moveBackward, rotateClockwise, rotateCounterclockwise;
-	private Pool<Bolt> boltPool;
 	private final Player me;
 	
 	/**
@@ -31,40 +30,40 @@ public class InputManager extends InputListener {
 	public InputManager(final Player localPlayer) {
 		if(!localPlayer.isLocalPlayer())
 			throw new IllegalArgumentException("Must be the local player");
-		boltPool = Pools.get(Bolt.class);
 		me = localPlayer;
 	}
 	
 	@Override
 	public boolean keyDown(InputEvent event, int keycode) {
+		float deltaTime = Gdx.graphics.getDeltaTime();
 		switch(keycode)
 		{
 		case Keys.W: //move the player forward
-			final MoveByAction moveByForward = Actions.moveBy(me.getSpeedCompX(), me.getSpeedCompY(), 0.01f, Interpolation.linear);
+			final MoveByAction moveByForward = Actions.moveBy(me.getSpeedCompX() * deltaTime, me.getSpeedCompY() * deltaTime, 0.01f, Interpolation.linear);
 			Action runnableForward = Actions.run(() -> {
-				moveByForward.setAmount(me.getSpeedCompX(), me.getSpeedCompY());
+				moveByForward.setAmount(me.getSpeedCompX() * deltaTime, me.getSpeedCompY() * deltaTime);
 			});
 			moveForward = Actions.forever(Actions.parallel(moveByForward, runnableForward));
 			me.addAction(moveForward);
 			return true;
 		case Keys.A: //rotate the player counter clockwise
-			rotateCounterclockwise = Actions.forever(Actions.rotateBy(Player.ROTATE_SPEED, 0.01f, Interpolation.linear));
+			rotateCounterclockwise = Actions.forever(Actions.rotateBy(Player.ROTATE_SPEED * deltaTime, 0.01f, Interpolation.linear));
 			me.addAction(rotateCounterclockwise);
 			return true;
 		case Keys.S: //move the player backward
-			final MoveByAction moveByBackward = Actions.moveBy(-me.getSpeedCompX() * 0.25f, -me.getSpeedCompY() * 0.25f, 0.01f, Interpolation.linear);
+			final MoveByAction moveByBackward = Actions.moveBy(-me.getSpeedCompX() * deltaTime * 0.25f, -me.getSpeedCompY() * deltaTime * 0.25f, 0.01f, Interpolation.linear);
 			Action runnableBackward = Actions.run(() -> {
-				moveByBackward.setAmount(-me.getSpeedCompX() * 0.25f, -me.getSpeedCompY() * 0.25f);
+				moveByBackward.setAmount(-me.getSpeedCompX() * deltaTime * 0.25f, -me.getSpeedCompY() * deltaTime * 0.25f);
 			});
 			moveBackward = Actions.forever(Actions.parallel(moveByBackward, runnableBackward));
 			me.addAction(moveBackward);
 			return true;
 		case Keys.D: //rotate the player clockwise
-			rotateClockwise = Actions.forever(Actions.rotateBy(-Player.ROTATE_SPEED, 0.01f, Interpolation.linear));
+			rotateClockwise = Actions.forever(Actions.rotateBy(-Player.ROTATE_SPEED * deltaTime, 0.01f, Interpolation.linear));
 			me.addAction(rotateClockwise);
 			return true;
 		case Keys.SPACE: //fire a bolt
-			Bolt bolt = boltPool.obtain().setShooterId(ServerInterface.getClientId()).setSpeed(me.getSpeed() + 1f);
+			Bolt bolt = Pools.get(Bolt.class).obtain().setShooterId(ServerInterface.getClientId()).setSpeed(me.getSpeed() + 1f);
 			Vector2 boltSpawn = new Vector2(me.getOriginX(), 0);
 			boltSpawn.rotate(me.getRotation());
 			boltSpawn.add(me.getX() + me.getOriginX(), me.getY() + me.getOriginY());
@@ -79,18 +78,19 @@ public class InputManager extends InputListener {
 	
 	@Override
 	public boolean keyUp(InputEvent event, int keycode) {
+		float deltaTime = Gdx.graphics.getDeltaTime();
 		switch(keycode)
 		{
 		case Keys.W:
 			moveForward.finish();
-			me.addAction(Actions.moveBy(me.getSpeedCompX() * 10, me.getSpeedCompY() * 10, 1.5f, Interpolation.pow5Out));
+			me.addAction(Actions.moveBy(me.getSpeedCompX() * deltaTime * 10, me.getSpeedCompY() * deltaTime * 10, 1.5f, Interpolation.pow5Out));
 			return true;
 		case Keys.A:
 			rotateCounterclockwise.finish();
 			return true;
 		case Keys.S:
 			moveBackward.finish();
-			me.addAction(Actions.moveBy(-me.getSpeedCompX() * 2.5f, -me.getSpeedCompY() * 2.5f, 1.5f, Interpolation.pow5Out));
+			me.addAction(Actions.moveBy(-me.getSpeedCompX() * deltaTime * 2.5f, -me.getSpeedCompY() * deltaTime * 2.5f, 1.5f, Interpolation.pow5Out));
 			return true;
 		case Keys.D:
 			rotateClockwise.finish();
